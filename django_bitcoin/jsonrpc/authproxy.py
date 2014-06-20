61f7f708de001c46cc6875cea1feacea8720d735
Andrii Kostenko <andrey@kostenko.name>
2014-06-20 17:44:46 +0300

    Python 3 bytearray support

--- django_bitcoin/jsonrpc/authproxy.py
+++ django_bitcoin/jsonrpc/authproxy.py
@@ -60,9 +60,9 @@ class AuthServiceProxy(object):
         else:
             port = self.__url.port
         self.__idcnt = 0
+        authpair = "%s:%s" % (self.__url.username, self.__url.password)
         if sys.version_info >= (3, 0):
           authpair = bytearray(authpair, "utf8")
-        authpair = "%s:%s" % (self.__url.username, self.__url.password)
         self.__authhdr = "Basic %s" % (base64.b64encode(authpair))
         if self.__url.scheme == 'https':
             self.__conn = httplib.HTTPSConnection(self.__url.hostname, port, None, None,False,
